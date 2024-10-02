b8f8da24354c2dd703b553f1457000690bc89920
Fox33m <100588720+Fox33m@users.noreply.github.com>
2024-10-02 10:31:03 +0200

    Update script.js

--- Sito primario/script.js
+++ Sito primario/script.js
@@ -1,1 +1,16 @@
 
+document.addEventListener("click", function(event) {
+  
+  const dropdownMenu = document.getElementById("dropdownMenu");
+
+  if (event.target.matches(".dropbtn")) {
+    
+      dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+    
+    } else {
+    
+      dropdownMenu.style.display = "none";
+    
+    }
+  
+});
